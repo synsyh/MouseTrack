01d0b50273a5f32ad1bc51c239d1958652594fc4
sunyuning <ssssssun@outlook.com>
2019-02-14 16:03:39 +0800

    增加了孪生神经网络的数据 修改了网络，可以测试和保存模型

--- siamese_preprocess.py
+++ siamese_preprocess.py
@@ -42,11 +42,11 @@ def get_batch():
 
     for iter, i in enumerate(k):
         if i < 25000:
-            line = linecache.getline('./data/siamese_data/siamese0_shuffle.txt', i+1)
+            line = linecache.getline('./data/siamese_data/siamese0_shuffle.txt', i + 1)
             label[iter] = 0
         else:
             i -= 25000
-            line = linecache.getline('./data/siamese_data/siamese1_shuffle.txt', i+1)
+            line = linecache.getline('./data/siamese_data/siamese1_shuffle.txt', i + 1)
             label[iter] = 1
         points1 = data_trans.analysis_data(line.split(' ')[0])
         points2 = data_trans.analysis_data(line.split(' ')[1])
@@ -62,33 +62,45 @@ def get_batch():
 def get_eval_batch():
     x_ratio = 0.27367970921530893
     y_ratio = 0.2158152082279548
-    k = random.sample(range(5000), 20)
     data = [np.zeros((20, 128, 128, 3)) for i in range(2)]
-    label = np.zeros((20,))
+    file_list = ['./data/siamese_data/sun2', './data/siamese_data/yu2', './data/siamese_data/yuan2']
+    true_file_num = random.choice(range(3))
+    file_path = file_list[true_file_num]
+    file_list.remove(file_path)
+
+    tmp_data = np.zeros((1, 128, 128, 3))
+    true_pair1, true_pair2 = random.choice(range(100), size=(2,))
+    line = linecache.getline(file_path, true_pair1)
+    points = data_trans.analysis_data(line)
+    points = get_velocity(points)
+    for point in points:
+        tmp_data[0][int(point['x'] * x_ratio)][int(point['y'] * y_ratio)] = [1, point['time'] / 100,
+                                                                             point['v']]
+    data[0] = tmp_data * 20
+
     true_num = random.choice(range(20))
-    label[true_num] = 1
-    true_category = random.randint(5000, 10000)
-    true_line = linecache.getline('./data/siamese_data/siamese_shuffle_eval', true_category)
-    true_points1 = data_trans.analysis_data(true_line.split(' ')[0])
-    true_points2 = data_trans.analysis_data(true_line.split(' ')[1])
-    true_points1 = get_velocity(true_points1)
-    true_points2 = get_velocity(true_points2)
-    for point in true_points1:
-        data[0][true_num][int(point['x'] * x_ratio)][int(point['y'] * y_ratio)] = [1, point['time'] / 100, point['v']]
-    for point in true_points2:
-        data[1][true_num][int(point['x'] * x_ratio)][int(point['y'] * y_ratio)] = [1, point['time'] / 100, point['v']]
-    data[0] = data[0][true_num] * 20
+    line = linecache.getline(file_path, true_pair2)
+    points = data_trans.analysis_data(line)
+    points = get_velocity(points)
+    for point in points:
+        data[1][true_num][int(point['x'] * x_ratio)][int(point['y'] * y_ratio)] = [1, point['time'] / 100,
+                                                                                   point['v']]
+
+    lines = []
+    for file in file_list:
+        with open(file, 'r') as f:
+            lines.append(f.readlines())
+    k = random.choice(range(200), size=(20,))
     for iter, i in enumerate(k):
         if iter == true_num:
             continue
-        line = linecache.getline('./data/siamese_data/siamese_shuffle_eval', i+1)
-        points1 = data_trans.analysis_data(line.split(' ')[0])
-        points2 = data_trans.analysis_data(line.split(' ')[1])
-        points1 = get_velocity(points1)
-        points2 = get_velocity(points2)
-        for point in points1:
-            data[0][iter][int(point['x'] * x_ratio)][int(point['y'] * y_ratio)] = [1, point['time'] / 100, point['v']]
-        for point in points2:
+        line = lines[i]
+        points = data_trans.analysis_data(line)
+        points = get_velocity(points)
+        for point in points:
             data[1][iter][int(point['x'] * x_ratio)][int(point['y'] * y_ratio)] = [1, point['time'] / 100, point['v']]
+
+    label = np.zeros((20,))
+    label[true_num] = 1
 
     return data, label
